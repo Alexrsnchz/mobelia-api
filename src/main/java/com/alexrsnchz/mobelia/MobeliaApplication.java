bc7b8fef38abce43bdc1c5b58edaa80d2b04b29e
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-03-23 01:15:42 +0100

    build: Project start (files upload)

---
+++ src/main/java/com/alexrsnchz/mobelia/MobeliaApplication.java
@@ -0,0 +1,13 @@
+package com.alexrsnchz.mobelia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobeliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobeliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexrsnchz/mobelia/MobeliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexrsnchz.mobelia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobeliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
